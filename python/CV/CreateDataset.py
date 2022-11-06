76b3bf4a8cc0dfa54f76ce9529c8f2884f64633c
JarneVk <jarne.vankerckhove24@gmail.com>
2022-11-06 18:08:58 +0100

    omzetten naar pytorch

--- python/CV/CreateDataset.py
+++ python/CV/CreateDataset.py
@@ -1,46 +1,41 @@
-import numpy as np
+import torch
+from torch.utils.data import Dataset
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+import matplotlib.pyplot as plt
 import os
-import cv2
-import random
+import pandas as pd
+from torchvision.io import read_image
+from torchvision.transforms import transforms
+import torch.utils.data 
 import joblib
 
-IMG_SIZE_W = 50 #for our camera 800 is full resolution
-IMG_SIZE_H = 50 #for our camera 600 is full resolution
+class CustomImageDataset(Dataset):
+    def __init__(self, annotations_file, img_dir, transform=None, target_transform=None):
+        self.img_labels = pd.read_csv(annotations_file)
+        self.img_dir = img_dir
+        self.transform = transform
+        self.target_transform = target_transform
 
-#@param data_dir : path to the main folder that contains the subfolders with categories
-#@param classes : subfolders of the main folders that represent the categories
-def createDataset(data_dir,classes):
-    training_data = []
-    for clas in classes:
-        path = os.path.join(data_dir,clas)
-        class_num = classes.index(clas) #binary representation
-        for img in os.listdir(path):
-            try:
-                im_mat = cv2.imread(os.path.join(path,img),cv2.IMREAD_GRAYSCALE) #read in grayscale because it takes in less memory and the images are saved in grayscale
-                im_norm = cv2.resize(im_mat,(IMG_SIZE_W,IMG_SIZE_H))
-                training_data.append([im_norm,class_num])
-            except Exception as e:
-                pass
-    
-    print('length loaded data:'+str(len(training_data)))
-    random.shuffle(training_data) #shuffle the data so its not all in the order of the categories
+    def __len__(self):
+        return len(self.img_labels)
 
-    train_x = []
-    test_y = []
+    def __getitem__(self, idx):
+        img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])
+        image = read_image(img_path)
+        label = self.img_labels.iloc[idx, 1]
+        if self.transform:
+            image = self.transform(image)
+        if self.target_transform:
+            label = self.target_transform(label)
+        return image, label
 
-    for features,label in training_data:
-        train_x.append(features)
-        test_y.append(label)
-    train_x = np.array(train_x).reshape(-1, IMG_SIZE_W, IMG_SIZE_H, 1) # tf expects a numpy array
+dataset = CustomImageDataset("test_data_cat_dogs/classes.csv","test_data_cat_dogs/Img_dataset",transform=transforms.ToTensor())
+print(len(dataset))
+test_aant = len(dataset)*75
+train_aant = len(dataset)-test_aant
+train_x, test_y = torch.utils.data.random_split(dataset,[train_aant,test_aant])
 
-    #save the data
-    joblib.dump(train_x,"python/CV/train_x.joblib")
-    joblib.dump(test_y,"python/CV/test_y.joblib")
-
-                
-
-
-data_dir = "test_data_cat_dogs/PetImages"
-classes = ["Dog","Cat"]
-
-createDataset(data_dir,classes)
+#save the data
+joblib.dump(train_x,"python/CV/train_x.joblib")
+joblib.dump(test_y,"python/CV/test_y.joblib")
